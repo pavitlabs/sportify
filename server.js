5e80d88b6404d692887c08da32d367d7fa28eff5
Sai <saihegde@gmail.com>
2014-06-07 21:05:59 -0600

    Game Module changes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var app = require('./server/config/system/bootstrap')(passport, db);
 
 // Start the app by listening on <port>, optional hostname
 app.listen(config.port, config.hostname);
-console.log('Mean app started on port ' + config.port + ' (' + process.env.NODE_ENV + ')');
+console.log('Sportify started on port ' + config.port + ' (' + process.env.NODE_ENV + ')');
 
 // Initializing logger
 logger.init(app, passport, mongoose);
